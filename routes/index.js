35a33cc5bc898b64d5d34af02666773b624e2f0d
nipon pantong <nipon.pantong@scb.co.th>
2019-02-28 00:09:42 +0700

    Create save path route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/', require('./savePath'));
 router.get('/cost/:path', require('./costByPath'));
 
 module.exports = router;
