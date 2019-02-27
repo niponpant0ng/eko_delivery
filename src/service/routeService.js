dd77cf9c4ef844772be48af3e270ce9f1d2e22a6
nipon pantong <nipon.pantong@scb.co.th>
2019-02-28 01:09:02 +0700

    Edit save to support many edge

--- src/service/routeService.js
+++ src/service/routeService.js
@@ -5,13 +5,15 @@ const routeStorage = new NodeCache();
 module.exports.save = (paths) => {
   const route = new Route()
 
-  const nodeFrom = paths.substring(0, 1)
-  const nodeTo = paths.substring(1, 2)
-  const weight = parseInt(paths.substring(2, 3))
+  paths.split(', ').forEach(path => {
+    const nodeFrom = path.substring(0, 1)
+    const nodeTo = path.substring(1, 2)
+    const weight = parseInt(path.substring(2, 3))
 
-  route.addNode(nodeFrom)
-  route.addNode(nodeTo)
-  route.addEdge(nodeFrom, nodeTo, weight)
+    route.addNode(nodeFrom)
+    route.addNode(nodeTo)
+    route.addEdge(nodeFrom, nodeTo, weight)
+  })
 
   routeStorage.set('route', route)
 
--- test/service/routeService.test.js
+++ test/service/routeService.test.js
@@ -8,5 +8,22 @@ describe('route service', () => {
       expect(route.adjacent("A")).toHaveLength(1)
       expect(route.adjacent("A")[0]).toMatchObject({ node: 'B', weight: 1 })
     })
+
+    test('Should save route from A to B with weight is 1 and from C to D with weight is 3', () => {
+      const route = save('AB1, CD3')
+
+      expect(route.adjacent("A")).toHaveLength(1)
+      expect(route.adjacent("A")[0]).toMatchObject({ node: 'B', weight: 1 })
+      expect(route.adjacent("C")).toHaveLength(1)
+      expect(route.adjacent("C")[0]).toMatchObject({ node: 'D', weight: 3 })
+    })
+
+    test('Should save route from A to B with weight is 1 and from A to D with weight is 3', () => {
+      const route = save('AB1, AD3')
+
+      expect(route.adjacent("A")).toHaveLength(2)
+      expect(route.adjacent("A")[0]).toMatchObject({ node: 'B', weight: 1 })
+      expect(route.adjacent("A")[1]).toMatchObject({ node: 'D', weight: 3 })
+    })
   })
 })
