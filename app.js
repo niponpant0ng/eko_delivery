9453a1d0591af00aee4ca315ddc5152784e3239b
nipon pantong <nipon.pantong@scb.co.th>
2019-02-28 00:08:44 +0700

    Create cost by path route

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -15,9 +12,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/routes', require('./routes'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/costByPath.js
@@ -0,0 +1,3 @@
+module.exports = (req, res) => {
+  res.send('Cost by path')
+}
