130c1295b71d297486973c03717dcbcf16c8b990
nipon pantong <nipon.pantong@scb.co.th>
2019-03-01 10:03:50 +0700

    Add chepest end point

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ router.post('/', require('./savePath'));
 router.get('/cost/:path', require('./costByPath'));
 router.get('/:path', require('./posiblePath'));
 router.get('/:path/limit/:limit', require('./posiblePathWithLimit'));
+router.get('/chepest/:path', require('./chepestPath'));
 
 module.exports = router;
